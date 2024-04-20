a24bcd33e5775a7984250cc5539e9c547673ea88
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-04-19 23:00:43 -0300

    add check test

--- tests.sh
+++ tests.sh
@@ -3,6 +3,12 @@
 # Path to the woche.sh script
 woche_script_path="./woche.sh"
 
+check_test_result() {
+    if [[ "$output" == *"FAILED"* ]]; then
+        exit 1
+    fi
+}
+
 # Test with more than 2 arguments
 output=$("$woche_script_path" mon "Test task" "Extra argument")
 if [[ "$output" == *"tips: woche.sh"* ]]; then
@@ -10,14 +16,16 @@ if [[ "$output" == *"tips: woche.sh"* ]]; then
 else
     echo "Test 'more than 2 arguments' command: FAILED"
 fi
+check_test_result
 
 # Test the 'help' command
 output=$("$woche_script_path" help)
 if [[ "$output" == *"tips: woche.sh"* ]]; then
-  echo "Test 'help' command: PASSED"
+    echo "Test 'help' command: PASSED"
 else
-  echo "Test 'help' command: FAILED"
+    echo "Test 'help' command: FAILED"
 fi
+check_test_result
 
 # Test the 'create' command
 output=$("$woche_script_path" create)
@@ -26,6 +34,7 @@ if [[ "$output" == *"The file"*"has been created."* ]]; then
 else
     echo "Test 'create' command: FAILED"
 fi
+check_test_result
 
 # Test add task to a day command
 output=$("$woche_script_path" mon "Test task")
@@ -34,4 +43,5 @@ if [[ "$output" == *"Task 'Test task' added to"* ]]; then
 else
     echo "Test 'add task to a day' command: FAILED"
 fi
+check_test_result
 

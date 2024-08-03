70057fd6f3dad1502de8a88dc5e6e5bf1f6431dd
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-08-03 10:43:15 -0300

    Refactor to reuse file exists method using file variable

--- functions.sh
+++ functions.sh
@@ -5,6 +5,7 @@ tips() {
     echo "create: a new markdown file for the current week"
     echo "${week_array_string[@]}: to add a task to the day of the week."
     echo "show: show the tasks for the current week"
+    echo "show YYMMDD: show the tasks for the week starting on YYMMDD"
     echo "delete X: delete a task from the current week - X is the line number"
     echo "edit X: edit a task from the current week - X is the line number"
     echo "all: show all markdown files in the current directory"
@@ -16,26 +17,24 @@ start_day_of_week() {
     if [ "$(date "+%u")" == 1 ]; then
         start_day=$(date "+%y%m%d")
     fi
-
-    start_day_formatted=$(date -d "$start_day" "+%d/%m/%Y")
 }
 
 file_exists() {
-    if [ ! -e "$start_day.md" ]; then
-        echo "Error: The file $start_day.md does not exist."
+    if [ ! -e "$file.md" ]; then
+        echo "Error: The file $$file.md does not exist."
         exit 1
     fi
 }
 
 file_already_exists() {
-    if [ -e "$start_day.md" ]; then
-        echo "Error: The file $start_day.md already exists."
+    if [ -e "$file.md" ]; then
+        echo "Error: The file $$file.md already exists."
         exit 1
     fi
 }
 
 line_exists() {
-    if [ -z "$(sed -n "${task}p" "$start_day.md")" ]; then
+    if [ -z "$(sed -n "${task}p" "$file.md")" ]; then
         echo "Error: Line $task does not exist."
         exit 1
     fi
@@ -43,23 +42,25 @@ line_exists() {
 
 create_file() {
     for i in "${week_array[@]}"; do
-        printf "# %s\n\n" "$i" >> "$start_day.md"
+        printf "# %s\n\n" "$i" >> "$file.md"
     done
-    echo "The file $start_day.md has been created."
+    echo "The file $file.md has been created."
 }
 
 delete_line() {
-    sed -i "${task}d" "$start_day.md"
+    sed -i "${task}d" "$file.md"
     echo "Line ${task} deleted."
 }
 
 edit_line() {
-    sed -i "${task}s/.*/- $new_task/" "$start_day.md"
+    sed -i "${task}s/.*/- $new_task/" "$file.md"
     echo "Line ${task} edited."
 }
 
 show_file() {
-    cat -n "$start_day.md"
+    start_day_formatted=$(date -d "$file" "+%d/%m/%Y")
+    printf "Week starts on $start_day_formatted.\n\n"
+    cat -n "$file".md
 }
 
 show_all_files() {

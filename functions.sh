02b7f77dc17e234070b68353962f508c54a2ea0b
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-10-20 11:44:40 -0300

    Add last_week funciton and add the correct number of the day when create the week file

--- functions.sh
+++ functions.sh
@@ -6,6 +6,7 @@ tips() {
     echo "${week_array_string[@]}: to add a task to the day of the week."
     echo "show: show the tasks for the current week"
     echo "show YYMMDD: show the tasks for the week starting on YYMMDD"
+    echo "show last: show the tasks for last week"
     echo "delete X: delete a task from the current week - X is the line number"
     echo "edit X: edit a task from the current week - X is the line number"
     echo "all: show all markdown files in the current directory"
@@ -17,6 +18,10 @@ current_week() {
     if [ "$(date "+%u")" == 1 ]; then
         current_week=$(date "+%y%m%d")
     fi
+}
+
+last_week() {
+    last_week=$(date -d "$current_week - 7 days" "+%y%m%d")
 }
 
 file_exists() {
@@ -41,10 +46,25 @@ line_exists() {
 }
 
 create_file() {
-    for i in "${week_array[@]}"; do
-        printf "# %s\n\n" "$i" >> "$file.md"
-    done
+    create_week
     echo "The file $file.md has been created."
+}
+
+create_week() {
+    day_of_month=$(date -d "$current_week" "+%d" | sed 's/^0*//')
+    counter=0
+    days_of_month=$(( $(date -d "$(date -d "$current_week" "+%Y-%m-01") +1 month -1 day" "+%d") ))
+    current_month=$(( $(date -d "$current_week" "+%m") ))
+    month=$current_month
+    for i in "${week_array[@]}"; do
+        day_sum=$((day_of_month + counter))
+        if [ $day_sum -gt $days_of_month ]; then
+            day_sum=$((day_sum - days_of_month))
+            month=$((current_month + 1))
+        fi
+        printf "# %s\n\n" "$i, $day_sum/$month" >> "$file.md"
+        ((counter++))
+    done
 }
 
 delete_line() {
@@ -60,8 +80,8 @@ edit_line() {
 
 show_file() {
     start_day_formatted=$(date -d "$file" "+%d/%m/%Y")
-    printf "Week starts on $start_day_formatted.\n\n"
-    cat -n "$file".md
+    printf "Week starts on %s.\n\n" "$start_day_formatted"
+    cat -n "$file.md"
 }
 
 show_all_files() {

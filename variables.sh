0f98c34c2a6f393b687d5df010274c39ec8528c8
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2025-01-12 16:37:12 -0300

    [27] Export variables

--- variables.sh
+++ variables.sh
@@ -4,28 +4,28 @@
 path_to_files="/home/$(whoami)/"
 
 # Days of the week in German and English
-mont="Montag"
-die="Dienstag"
-mit="Mittwoch"
-don="Donnerstag"
-fre="Freitag"
-sam="Samstag"
-son="Sonntag"
+export mont="Montag"
+export die="Dienstag"
+export mit="Mittwoch"
+export don="Donnerstag"
+export fre="Freitag"
+export sam="Samstag"
+export son="Sonntag"
 
-mon="Monday"
-tue="Tuesday"
-wed="Wednesday"
-thu="Thursday"
-fri="Friday"
-sat="Saturday"
-sun="Sunday"
+export mon="Monday"
+export tue="Tuesday"
+export wed="Wednesday"
+export thu="Thursday"
+export fri="Friday"
+export sat="Saturday"
+export sun="Sunday"
 
-woche_array=("$mont" "$die" "$mit" "$don" "$fre" "$sam" "$son")
-week_array=("$mon" "$tue" "$wed" "$thu" "$fri" "$sat" "$sun")
+export woche_array=("$mont" "$die" "$mit" "$don" "$fre" "$sam" "$son")
+export week_array=("$mon" "$tue" "$wed" "$thu" "$fri" "$sat" "$sun")
 
-woche_array_string=("mont" "die" "mit" "don" "fre" "sam" "son")
-week_array_string=("mon" "tue" "wed" "thu" "fri" "sat" "sun")
-options=("create" "show" "help" "delete" "edit" "all")
+export woche_array_string=("mont" "die" "mit" "don" "fre" "sam" "son")
+export week_array_string=("mon" "tue" "wed" "thu" "fri" "sat" "sun")
+export options=("create" "show" "help" "delete" "edit" "all")
 
 # Create an array that is options + woche_array name of variables as strings
-options_to_check=("${options[@]}" "${week_array_string[@]}")
+export options_to_check=("${options[@]}" "${week_array_string[@]}")
